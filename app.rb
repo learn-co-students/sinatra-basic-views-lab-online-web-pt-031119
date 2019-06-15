bf60c65496abfe014a6180032c4adf43934ad1fc
Kat B <katreesebarnes@gmail.com>
2019-06-15 19:18:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
+	get '/' do
+      erb :index
+    end
+    
+  #   get '/' do
+  #   <h1>Buster's Best Bowling Alley</h1> 
+  # end
+  
 end
